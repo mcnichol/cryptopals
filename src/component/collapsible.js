77a31fba9f866e39bd315d864cac91188801c033
Michael McNichol <mcnichol.m@gmail.com>
2019-01-01 21:25:04 -0600

    Logic to collapsible component, style sections

--- src/component/collapsible.js
+++ src/component/collapsible.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import './collapsible.css';
 
 class Collapsible extends Component {
   constructor(props){
@@ -11,6 +12,8 @@ class Collapsible extends Component {
   }
 
   handleToggle(e) {
+    if(e.target.target === "_blank") return;
+
     e.preventDefault();
     this.setState({
       isExpanded: !this.state.isExpanded
@@ -19,14 +22,18 @@ class Collapsible extends Component {
 
   render(){
     const {linkId, title, children} = this.props;
+    const isExpanded = this.state.isExpanded;
 
     return (
-      <div className="panel" onClick={(e)=>this.handleToggle(e)}>
-        <div className="panel-heading">
-          <h3 id={linkId}>{title}</h3>
+      <div>
+        <div className="panel" onClick={(e)=>this.handleToggle(e)}>
+          <div className="panel-heading">
+            <h3 id={linkId}>{title}</h3>
+          </div>
         </div>
+
         <div className="panel-collapse">
-          <div className="panel-body">
+          <div className={`panel-body ${isExpanded ? 'is-expanded' : ''}`}>
             {children}
           </div>
         </div>
--- src/component/s1c1.js
+++ src/component/s1c1.js
@@ -4,6 +4,11 @@ import './s1c1.css'
 
 class S1C1 extends Component {
 
+  constructor(props){
+    super(props);
+
+  }
+
   render(){
 
     return (
@@ -14,7 +19,6 @@ class S1C1 extends Component {
           User must convert this hex value:
         </p>
         <code>49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d</code>
-
         <p>
           to this base64 value:
         </p>
@@ -28,13 +32,13 @@ class S1C1 extends Component {
           This is a gentle on-ramp helping developers shine light on the fact that flipping from various encodings and working on the raw bytes will be a key aspect in building our understanding from the ground up.
         </p>
         <p>A few things to note:</p>
-        <ul>
+        <ol>
           <li><a href="#note1">Hex is base 16, base64 aptly named is base 64</a></li>
           <li>Hex encoded ASCII values utilize one octet for each ASCII character</li>
           <li>Each Hex value is a nibble whereas base64 operates in sextets. This means 3 Hex values (3x4=12 bits) can be expressed with two sextets (2x6=12 bits)</li>
           <li>A more human readable form would be ASCII and an easy solvable view would be binary, but neither are required intermediaries in order to convert directly from Hex to base64</li>
-        </ul>
-        <Collapsible linkId="note1" title="Hex is base16, base64 is base64">
+        </ol>
+        <Collapsible linkId="note1" title="1. Hex is base16, base64 is base64">
           <p>
             Hex being base 16 is more precisely referred to as <a href="https://en.wikipedia.org/wiki/Hexadecimal" target="_blank" rel="noopener noreferrer">Hexadecimal</a>.  Hex (6) && Decimal (10).  This means that there are 16 possible values before we roll over to 10.  Compare this to <a href="https://en.wikipedia.org/wiki/Binary_number" target="_blank" rel="noopener noreferrer">binary</a> where there are only 2 values (1 || 0) before we roll over to 10. For reference that most are likely familiar with, decimal values number 0 - 9 before we roll over to 10.
           </p>
