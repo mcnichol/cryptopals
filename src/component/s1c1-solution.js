5831b3f25290398503e5dd9f8b33520ad3e11688
Michael McNichol <mcnichol.m@gmail.com>
2019-01-07 23:33:40 -0600

    Style solution

--- src/component/s1c1-solution.js
+++ src/component/s1c1-solution.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import "./s1c1-solution.css";
 
 class S1C1Solution extends Component {
 
@@ -7,16 +8,21 @@ class S1C1Solution extends Component {
 
     this.state = {
       hexInput: "",
-      base64LocalJS: "Empty",
-      base64Go: "Empty",
-      base64Java: "Empty",
-      base64JS: "Empty",
-      base64Python: "Empty",
+      base64LocalJS: "",
+      base64Go: "",
+      base64Java: "",
+      base64JS: "",
+      base64Python: "",
+      language: "local-js"
     }
   }
 
   localSolution(hex){
-    let base64EncodedVal = btoa(hex.match(/\w{2}/g).map(function(x){return String.fromCharCode(parseInt(x, 16));}).join(""));
+    let base64EncodedVal = "";
+
+    if(hex !== ""){
+      base64EncodedVal = btoa(hex.match(/\w{2}/g).map(function(x){return String.fromCharCode(parseInt(x, 16));}).join(""));
+    }
 
     this.setState({
       base64LocalJS: base64EncodedVal
@@ -31,25 +37,41 @@ class S1C1Solution extends Component {
     });
   }
 
+  handleLanguageToggle(langVal){
+    this.setState({
+      language: langVal
+    });
+  }
+
   render(){
     return (
-      <div>
+      <div className="solution">
         <h2>Solution</h2>
-        <div><b>Language:</b>
-          <div id="lang-local-js">Local JavaScript</div>
-          <div id="lang-go">Go</div>
-          <div id="lang-java">Java</div>
-          <div id="lang-js">Node/Express.js</div>
-          <div id="lang-python">Python</div>
+        <div id="choose-lang" className="selection">
+          <h4>Select Language:</h4>
+          <ul className="selection-container">
+            <li id="lang-local-js" className={`${this.state.language === 'local-js' ? 'is-active' : ''}`} onClick={(()=>{this.handleLanguageToggle("local-js")})}>Local JS</li>
+            <li id="lang-go" className={`${this.state.language === 'go' ? 'is-active' : ''}`} onClick={(()=>{this.handleLanguageToggle("go")})}>Go</li>
+            <li id="lang-java" className={`${this.state.language === 'java' ? 'is-active' : ''}`} onClick={(()=>{this.handleLanguageToggle("java")})}>Java</li>
+            <li id="lang-js" className={`${this.state.language === 'js' ? 'is-active' : ''}`} onClick={(()=>{this.handleLanguageToggle("js")})}>Node/Express.js</li>
+            <li id="lang-python" className={`${this.state.language === 'python' ? 'is-active' : ''}`} onClick={(()=>{this.handleLanguageToggle("python")})}>Python</li>
+          </ul>
         </div>
-        <div><b>IaaS:</b>
-          <div id="iaas-aws">AWS</div>
-          <div id="iaas-azure">Azure</div>
-          <div id="iaas-gcp">GCP</div>
-          <div id="iaas-vsphere">vSphere</div>
+        <div id="choose-iaas" className="selection">
+          <h4>Select IaaS:</h4>
+          <ul className="selection-container">
+            <li><div id="iaas-aws">AWS</div></li>
+            <li><div id="iaas-azure">Azure</div></li>
+            <li><div id="iaas-gcp">GCP</div></li>
+            <li><div id="iaas-vsphere">vSphere</div></li>
+          </ul>
         </div>
-        <p>Hex Input: <input value={this.state.hexInput} onChange={((e)=>{this.handleInput(e)})}/><button onClick={(()=>{this.localSolution(this.state.hexInput)})}>Submit</button></p>
-        <div>base64 Output:
+        <br/>
+        <div>
+          <p><b>Hex Input:</b><input value={this.state.hexInput} onChange={((e)=>{this.handleInput(e)})}/><button onClick={(()=>{this.localSolution(this.state.hexInput)})}>Submit</button></p>
+        </div>
+        <div>
+          <b>base64 Output:</b>
           <div id="base64-local-js"><code>{this.state.base64LocalJS}</code></div>
           <div id="base64-go"><code>{this.state.base64Go}</code></div>
           <div id="base64-java"><code>{this.state.base64Java}</code></div>
